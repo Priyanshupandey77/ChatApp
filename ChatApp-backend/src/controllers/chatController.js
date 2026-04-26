39f6c7c9ff6fa8c75c8488e519de1948551eb14b
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-04-27 00:59:21 +0530

    Add one-to-one chat system with create/access API

---
+++ ChatApp-backend/src/controllers/chatController.js
@@ -0,0 +1,38 @@
+import Chat from "../models/Chat.js";
+
+export const CreateOrAccessChat = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const currentUser = req.user._id;
+
+    if (!userId) {
+      return res.status(400).json({ msg: "userId is required" });
+    }
+
+    if (currentUser.toString() === userId) {
+      return res.status(400).json({ msg: "userid cannot be of currentUser" });
+    }
+
+    const chat = await Chat.findOne({
+      isGroup: false,
+      users: { $all: [currentUser, userId] },
+    }).populate("users", "-password");
+
+    if (chat) {
+      return res.json(chat);
+    }
+
+    const newChat = await Chat.create({
+      users: [currentUser, userId],
+      isGroup: false,
+    });
+
+    const fullChat = await Chat.findById(newChat._id).populate(
+      "users",
+      "-password",
+    );
+    return res.status(201).json(fullChat);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
--- ChatApp-backend/src/server.js
+++ ChatApp-backend/src/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { protect } from "./middleware/authMiddleware.js";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
-
+import chatRoutes from "./routes/chatRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
-
+app.use("/api/chat", chatRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
