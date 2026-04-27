0f4a44d5ebab48952790ff8dc5fe195a08bb1701
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-04-27 10:38:28 +0530

    implement secure messaging with send and fetch APIs

---
+++ ChatApp-backend/src/controllers/messageController.js
@@ -0,0 +1,78 @@
+import Chat from "../models/Chat.js";
+import Message from "../models/Message.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { content, chatId } = req.body;
+    const sender = req.user._id;
+    if (!content) {
+      return res.status(400).json({ msg: "content is required" });
+    }
+    if (!chatId) {
+      return res.status(400).json({ msg: "chatId not found" });
+    }
+
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+      return res.status(404).json({ msg: "chat not found" });
+    }
+    const isUserInChat = chat.users.some(
+      (user) => user.toString() === req.user._id.toString(),
+    );
+    if (!isUserInChat) {
+      return res.status(403).json({ msg: "Not authorized" });
+    }
+    const message = await Message.create({
+      sender,
+      chat: chatId,
+      content,
+      seenBy: [sender],
+    });
+
+    await Chat.findByIdAndUpdate(chatId, {
+      lastMessage: message._id,
+    });
+
+    await message.populate([
+      { path: "chat", select: "users" },
+      { path: "sender", select: "name email" },
+    ]);
+
+    return res.status(201).json({ ...message._doc, __v: undefined });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { chatId } = req.params;
+    if (!chatId) {
+      return res.status(400).json({ msg: "chatId is required" });
+    }
+
+    const chat = await Chat.findById(chatId);
+
+    if (!chat) {
+      return res.status(404).json({ msg: "chat not found" });
+    }
+
+    const isUserInChat = chat.users.some(
+      (user) => user.toString() === req.user._id.toString(),
+    );
+
+    if (!isUserInChat) {
+      return res.status(403).json({ msg: "Not authorized" });
+    }
+
+    const messages = await Message.find({ chat: chatId })
+      .populate("sender", "name email")
+      .populate("chat", "users")
+      .select("-__v")
+      .sort({ createdAt: 1 });
+
+    return res.status(200).json(messages);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
--- ChatApp-backend/src/server.js
+++ ChatApp-backend/src/server.js
@@ -5,6 +5,7 @@ import { protect } from "./middleware/authMiddleware.js";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
